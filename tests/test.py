3eb85f78268a305d7951aab9c99cd03be14ca8e5
Luis López <ldotlopez@gmail.com>
2018-04-09 17:45:49 +0200

    Update dt tests

--- tests/test.py
+++ tests/test.py
@@ -1,51 +1,54 @@
 import unittest
 
-import datetime
+from datetime import datetime
+from pathlib import Path
 
-from housekeeper import filesystem as hkfs
-from ocdutils import ocdphotos
+from ocdutils import (
+    dt as ocddt,
+    filesystem as ocdfs
+)
 
 
-class PhotosTest(unittest.TestCase):
+class DatetimeTest(unittest.TestCase):
     def test_name_set_op(self):
-        x = ocdphotos.NameHandler(format='foo-%Y%m%d.%H%M%S')
-        dt = datetime.datetime(year=1992, month=1, day=2,
-                               hour=3, minute=4, second=5)
+        x = ocddt.NameHandler(format='foo-%Y%m%d.%H%M%S')
+        dt = datetime(year=1992, month=1, day=2,
+                      hour=3, minute=4, second=5)
 
-        op = x.set('/path/foo.ext', dt)
-        self.assertTrue(isinstance(op, hkfs.RenameOperation))
-        self.assertEqual(op.dest, "/path/foo-19920102.030405.ext")
+        op = x.set(Path('/path/foo.ext'), dt)
+        self.assertTrue(isinstance(op, ocdfs.RenameOperation))
+        self.assertEqual(op.dest, Path("/path/foo-19920102.030405.ext"))
 
     def test_mtime_set_op(self):
-        x = ocdphotos.MtimeHandler(set_atime=True)
-        dt = datetime.datetime(year=1992, month=1, day=2,
-                               hour=3, minute=4, second=5)
+        x = ocddt.MtimeHandler(set_atime=True)
+        dt = datetime(year=1992, month=1, day=2,
+                      hour=3, minute=4, second=5)
 
-        op = x.set('/path/foo.ext', dt)
-        self.assertTrue(isinstance(op, hkfs.SetTimestampOperation))
+        op = x.set(Path('/path/foo.ext'), dt)
+        self.assertTrue(isinstance(op, ocdfs.SetTimestampOperation))
         self.assertEqual(int(op.timestamp), 694317845)
         self.assertEqual(op.set_mtime, True)
         self.assertEqual(op.set_atime, True)
 
     def test_exif_set_op(self):
-        x = ocdphotos.ExifHandler()
-        dt = datetime.datetime(year=1992, month=1, day=2,
-                               hour=3, minute=4, second=5)
+        x = ocddt.ExifHandler()
+        dt = datetime(year=1992, month=1, day=2,
+                      hour=3, minute=4, second=5)
 
-        op = x.set('/path/foo.ext', dt)
-        self.assertTrue(isinstance(op, hkfs.CustomOperation))
+        op = x.set(Path('/path/foo.ext'), dt)
+        self.assertTrue(isinstance(op, ocdfs.CustomOperation))
         self.assertEqual(op.fn, x.write_exif_tag)
-        self.assertEqual(op.args, ('/path/foo.ext', dt))
+        self.assertEqual(op.args, (Path('/path/foo.ext'), dt))
 
     def test_exif_set_op_with_zero(self):
-        x = ocdphotos.ExifHandler()
-        dt = datetime.datetime(year=1992, month=1, day=2,
-                               hour=3, minute=4, second=0)
+        x = ocddt.ExifHandler()
+        dt = datetime(year=1992, month=1, day=2,
+                      hour=3, minute=4, second=0)
 
-        op = x.set('/path/foo.ext', dt)
-        self.assertTrue(isinstance(op, hkfs.CustomOperation))
+        op = x.set(Path('/path/foo.ext'), dt)
+        self.assertTrue(isinstance(op, ocdfs.CustomOperation))
         self.assertEqual(op.fn, x.write_exif_tag)
-        self.assertEqual(op.args, ('/path/foo.ext', dt))
+        self.assertEqual(op.args, (Path('/path/foo.ext'), dt))
 
 
 if __name__ == '__main__':
