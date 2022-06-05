8f108af3456e4d6c91496d6ec9445a45b8329859
Luis López <ldotlopez@gmail.com>
2022-06-05 11:39:14 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -26,26 +26,24 @@ import datetime
 
 
 setup(
-    name='ocdutils',
-    version='0.0.0.' + datetime.datetime.now().strftime('%Y%m%d%H%M%S'),
-    author='Luis López',
-    author_email='luis@cuarentaydos.com',
-    packages=['ocdutils'],
+    name="ocdutils",
+    version="1.0.0-alpha" + datetime.datetime.now().strftime("%Y%m%d%H%M%S"),
+    author="Luis López",
+    author_email="luis@cuarentaydos.com",
+    packages=["ocdutils"],
     scripts=[],
-    url='https://github.com/ldotlopez/ocdutils',
-    license='LICENSE.txt',
+    url="https://github.com/ldotlopez/ocdutils",
+    license="LICENSE.txt",
     description=(
         'Utils for control your "Obsessive-Compulsive Disorder" with your data'
     ),
-    long_description=open('README.md').read(),
-    install_requires=[
-        "piexif",
-        "pillow"
-    ],
+    long_description=open("README.md").read(),
+    install_requires=["piexif", "pillow", "py-dateutil"],
     entry_points={
-        'console_scripts':
-            ['ocd-dt-namer=ocdutils.dtnamer:main',
-             'ocd-uniq-namer=ocdutils.uniqnamer:main',
-             'ocd-fs-janitor=ocdutils.fsjanitor:main']
-    }
+        "console_scripts": [
+            "ocd-dt-namer=ocdutils.dtnamer:main",
+            "ocd-uniq-namer=ocdutils.uniqnamer:main",
+            "ocd-fs-janitor=ocdutils.fsjanitor:main",
+        ]
+    },
 )
